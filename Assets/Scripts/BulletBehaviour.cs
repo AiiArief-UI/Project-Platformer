c38f82e866caec370474ef2bb928b0f9ad050184
firas atha <vrasath@gmail.com>
2016-07-22 12:05:53 +0700

    control player

---
+++ Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletBehaviour : MonoBehaviour {
+	public float destroyTime;
+	void Start()
+	{
+		StartCoroutine("DestroyMe");
+	}
+
+	IEnumerator DestroyMe()
+	{
+		yield return new WaitForSeconds(destroyTime);
+		Destroy(gameObject);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (gameObject.CompareTag ("Bullet") && other.gameObject.CompareTag ("Player")) {
+			return;
+		} else if (gameObject.CompareTag ("EnemyBullet") && other.gameObject.CompareTag ("enemy")) {
+			return;
+		} else {
+			Destroy (gameObject);
+			Debug.Log ("ayy");
+		}
+	}
+}
